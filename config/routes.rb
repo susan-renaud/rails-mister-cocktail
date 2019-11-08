721256162bc2dbc9b16e9342cbe735059d34ca7c
Susan Renaud <serenaud89@gmail.com>
2019-11-08 15:25:53 -0600

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@
 
 Rails.application.routes.draw do #:nodoc"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "cocktails#index"
   resources :cocktails, only: [:index, :show, :new, :create] do
-    resources :doses, only: [:create, :new]
+    resources :doses, only: [:create]
     resources :reviews, only: [:create]
   end
   resources :doses, only: [:destroy]
-  root to: 'articles#index'
-  resources :articles, except: :index
 end
+
+
